62453de944f80c4e08dfa9504427f84ad9be8683
Clem Auyeung <clem.auyeung@gmail.com>
2018-06-28 11:32:54 -0400

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,4 +1,6 @@
 var = phrases [
+  "Hiya, I'm a content strategist, UX writer, and designer working at the intersection of editorial and product experience. I collaborate with teams to design the content that goes into websites, mobile apps, software, and conversational interfaces."
+  "I use the human-centered design process to help startups, nonprofits, and enterprise companies tackle gnarly content and product challenges. That usually means building empathy with the audience first with data, then iterating on ideas that make a real difference in people's lives."
   "Previously, I was a UX researcher at Capital One. I worked on AI research.",
   "I also designed a voice user interface for Capital One as a content designer.",
   "I received my M.S. in Human Centered Design & Engineering from the University of Washington.",
