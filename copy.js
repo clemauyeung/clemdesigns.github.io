7ff2ba9450dd5813f81b8577d566fa50e3de01d4
Clem Auyeung <clem.auyeung@gmail.com>
2018-06-28 10:38:02 -0400

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,1 +1,10 @@
+var = phrases [
+  "Previously, I was a UX researcher at Capital One. I worked on AI research.",
+  "I also designed a voice user interface for Capital One as a content designer.",
+  "I received my M.S. in Human Centered Design & Engineering from the University of Washington.",
+  "I'm grateful I had the chance to create an online course on community strategy with the wonderful friends at CMX Media.", 
+  "I also worked on building a community of listeners for a podcast on creativity.",
+  "I was an editor at one point.",
+  "I got my start at a tech startup helping organizations launch crowdfunding campaigns for social impact projects.",
+]
 
